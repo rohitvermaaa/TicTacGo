b44a7a95fc550daec1de83a985e94c0506a24560
ROHIT VERMA <rv329707@gmail.com>
2024-03-03 17:03:01 +0530

    Commit 3

--- app/src/main/java/com/example/tictacgo/GamescreenActivity.kt
+++ app/src/main/java/com/example/tictacgo/GamescreenActivity.kt
@@ -1,37 +1,30 @@
 package com.example.tictacgo
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import com.example.tictacgo.databinding.ActivityGamescreenBinding
 
 class GamescreenActivity : AppCompatActivity() {
-    private lateinit var sharedPrefs: SharedPreferences
     private lateinit var binding: ActivityGamescreenBinding
-
+    private var noOfXWins : Int = 0
+    private var noOfDraws : Int = 0
+    private var noOf0wins : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGamescreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        sharedPrefs = getSharedPreferences("MyPrefs", MODE_PRIVATE)
-        updateSwitch()
-
-        binding.uiModeSwitchCompat.setOnCheckedChangeListener { _, isChecked ->
-            AppCompatDelegate.setDefaultNightMode(
-                if (isChecked) AppCompatDelegate.MODE_NIGHT_NO else AppCompatDelegate.MODE_NIGHT_YES
-            )
-            saveMode(isChecked)
+        binding.btnResetScore.setOnClickListener {
+            resetScores()
         }
     }
 
-    private fun updateSwitch() {
-        val isDarkMode = sharedPrefs.getBoolean("isDarkMode", false)
-        binding.uiModeSwitchCompat.isChecked = isDarkMode
+    private fun resetScores() {
+        noOfXWins = 0
+        noOfDraws = 0
+        noOf0wins = 0
+        binding.tvNumberOfXWins.text = "$noOfXWins Wins"
+        binding.tvNumberOfDraws.text = "$noOfDraws Wins"
+        binding.tvNumberOfOWins.text = "$noOf0wins Wins"
     }
 
-    private fun saveMode(isDarkMode: Boolean) {
-        sharedPrefs.edit().putBoolean("isDarkMode", isDarkMode).apply()
-    }
 }
